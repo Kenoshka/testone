ca78f8e6510efe265fd3ca8ddfa28d93af8cc665
KenanGit <demirbagkenan@gmail.com>
2022-12-12 19:34:58 +0300

    Message

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GitBashLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите желаемую сумму: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double res = 0;
+            if (a < 100)
+            {
+                res = a + (a * 0.05);
+            }
+            else if (a < 200)
+            {
+                res = a + (a * 0.07);
+            }
+            else if (a >= 200)
+            {
+                res = a + (a * 0.1);
+            }
+            Console.WriteLine($"Итоговая сумма: {res}");
+        }
+    }
+}
